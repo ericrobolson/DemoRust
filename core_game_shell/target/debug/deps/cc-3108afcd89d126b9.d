1f0d99e84f25f705d5799d1a6256f7150183c403
eric.olson <eric.rob.olson@gmail.com>
2023-01-18 17:01:52 -0800

    Removed target.

--- core_game_shell/target/debug/deps/cc-3108afcd89d126b9.d
+++
@@ -1,8 +0,0 @@
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/cc-3108afcd89d126b9.rmeta: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/windows_registry.rs
-
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/libcc-3108afcd89d126b9.rlib: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/windows_registry.rs
-
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/cc-3108afcd89d126b9.d: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/windows_registry.rs
-
-/Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/lib.rs:
-/Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.78/src/windows_registry.rs:
--- core_game_shell/target/debug/deps/jobserver-3c74d822acf5acab.d
+++
@@ -1,8 +0,0 @@
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/jobserver-3c74d822acf5acab.rmeta: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/libjobserver-3c74d822acf5acab.rlib: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/Users/ericolson/dev2/blindsight/blindsight_core/target/debug/deps/jobserver-3c74d822acf5acab.d: /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs:
-/Users/ericolson/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs:
